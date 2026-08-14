273b79109621654b1ea45c703ef5fc5ead4cdb72
Ronaldo <ronaldoanthony12@gmail.com>
2026-08-14 13:03:04 -0400

    Do Spi bus and spi dev interface config

--- main/spi_pipeline/spi.c
+++ main/spi_pipeline/spi.c
@@ -12,6 +12,8 @@
 
 static const char *TAG = "SPI";
 
+spi_device_handle_t master_handle; 
+
 esp_err_t init_spi_bus(void) {
     spi_bus_config_t bus_conf = {
         .mosi_io_num = MOSI,
@@ -27,5 +29,23 @@ esp_err_t init_spi_bus(void) {
     
     CHECK_ERR(ret = spi_bus_initialize(SPI2_HOST, &bus_conf, SPI_DMA_CH_AUTO), return ret); //error checking
 
+    mutex_log('E', TAG, "SPI Physical Bus Initialized Successfully.");
+
     return ESP_OK;
+}
+
+esp_err_t init_spi_devs(void) {
+    spi_device_interface_config_t dev_conf = {
+        .clock_speed_hz = 1 * 1000 * 1000, //1mhz
+        .mode = 0,
+        .spics_io_num = CS, //cs gpio
+        .queue_size = 1, 
+    };
+
+    esp_err_t ret;
+
+    CHECK_ERR(ret = spi_bus_add_device(SPI2_HOST, &dev_conf, &master_handle), return ret);
+
+    mutex_log('I', TAG, "SPI Dev Interface Initialized Successfully.");
+
 }
