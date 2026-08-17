3146fa9728e659fdb3aa5a5d921f52734719ec35
Ronaldo <ronaldoanthony12@gmail.com>
2026-08-16 22:30:08 -0400

    dma async transaction setup

--- spi_master/main/spi_pipeline/spi_master.c
+++ spi_master/main/spi_pipeline/spi_master.c
@@ -58,6 +58,8 @@ esp_err_t init_spi_devs(void) {
 
 esp_err_t master_trasmit(void) {
 
+    size_t packet_size = 256;
+
     uint8_t tx_data[16] = {
         0x01, 0x02, 0x03, 0x04,
         0x05, 0x06, 0x07, 0x08,
@@ -65,20 +67,24 @@ esp_err_t master_trasmit(void) {
         0x0D, 0x0E, 0x0F, 0x10
     };
     
-    uint8_t *tx_buf = dma_alloc(256); //dma buf
+    uint8_t *tx_buf = dma_alloc(packet_size); //dma buf
     
     memcpy(tx_buf, tx_data, sizeof(tx_data));
     
-    uint8_t rx_buf[16] = {};
+    uint8_t *rx_buf = dma_alloc(packet_size); //expecting 256 bytes back
 
     spi_transaction_t _trans = {};
     _trans.tx_buffer = tx_buf;
     _trans.rx_buffer = rx_buf;
-    _trans.length = sizeof(tx_buf) * 8;
+    _trans.length = packet_size*8; //since 256 bytes per data packet, and for now im sending 8 packets
+
 
     esp_err_t ret;
 
-    CHECK_ERR(ret = spi_device_transmit(master_handle, &_trans), return ret);
+    spi_transaction_t* _trans_addr = &_trans; //ptr to trans for trans result
+
+    CHECK_ERR(ret = spi_device_queue_trans(master_handle, &_trans, portMAX_DELAY), return ret); //qeued for transmit
+    CHECK_ERR(ret = spi_device_get_trans_result(master_handle, &_trans_addr, portMAX_DELAY), return ret); //actual returned result
 
     mutex_log('I', TAG, "SPI Device Master Transmit Successful.");
 
@@ -86,4 +92,9 @@ esp_err_t master_trasmit(void) {
 
 
 
+}
+
+void trans_manage(uint8_t *buf, spi_transaction_t _trans) {
+    spi_device_queue_trans(master_handle, &_trans, portMAX_DELAY);
+    
 }
