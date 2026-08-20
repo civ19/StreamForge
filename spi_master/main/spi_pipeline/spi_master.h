abf63c17204b61a04c390e15c00d2726dd3d60ed
Ronaldo <ronaldoanthony12@gmail.com>
2026-08-20 10:43:35 -0400

    new helprs in master.h

--- spi_master/main/spi_pipeline/spi_master.h
+++ spi_master/main/spi_pipeline/spi_master.h
@@ -1,8 +1,14 @@
 #pragma once
 
 #include "esp_err.h"
+#include "spi_master.h"
 
 
 esp_err_t init_spi_bus(void);
 esp_err_t init_spi_devs(void);
 esp_err_t master_transmit_task(void);
+
+spi_transaction_t init_trans(uint8_t *tx_buf, uint8_t *rx_buf, size_t p_size);
+esp_err_t check_bufs(uint8_t* tx_buf, uint8_t* rx_buf, const char* msg);
+
+
