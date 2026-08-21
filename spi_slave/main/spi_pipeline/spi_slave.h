75f834e319e0c78abfda383bbe8434b81726c0ee
Ronaldo <ronaldoanthony12@gmail.com>
2026-08-21 10:58:43 -0400

    Completely refactor spi_slave.c

--- spi_slave/main/spi_pipeline/spi_slave.h
+++ spi_slave/main/spi_pipeline/spi_slave.h
@@ -6,6 +6,6 @@
 esp_err_t init_slave_bus(void);
 esp_err_t slave_transmit_task(void);
 
-spi_transaction_t init_trans(uint8_t *tx_buf, uint8_t *rx_buf, size_t p_size);
+spi_slave_transaction_t init_trans(uint8_t *tx_buf, uint8_t *rx_buf, size_t p_size);
 esp_err_t check_bufs(uint8_t* tx_buf, uint8_t* rx_buf, const char* msg);
 esp_err_t scale_buf_alloc(uint8_t** tx_buf, uint8_t** rx_buf, size_t n_bufs, size_t bytes);
