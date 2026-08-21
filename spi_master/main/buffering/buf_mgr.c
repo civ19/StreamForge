4c60e53322ba5c4cc034b3cf83969eeef64d758a
Ronaldo <ronaldoanthony12@gmail.com>
2026-08-20 22:05:32 -0400

    Create buffer struct

--- spi_master/main/buffering/buf_mgr.c
+++ spi_master/main/buffering/buf_mgr.c
@@ -1,2 +1,17 @@
 #include "buf_mgr.h"
 
+#include "freertos/FreeRTOS.h"
+#include "freertos/queue.h"
+#include "forge_err.h"
+#include "forge_log.h"
+
+#define PKT_SIZE 16 //16 bytes
+
+typedef struct {
+    int pos;
+    uint8_t data[PKT_SIZE];
+} Buffer;
+
+void buf_setup(void) {
+    
+}
