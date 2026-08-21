6954d2a986aa5150e071bcb06d3ece4ed4c0acfc
Ronaldo <ronaldoanthony12@gmail.com>
2026-08-20 21:03:18 -0400

    Successfully finish scaled spi and errros

--- spi_master/main/spi_pipeline/spi_master.h
+++ spi_master/main/spi_pipeline/spi_master.h
@@ -8,7 +8,9 @@ esp_err_t init_spi_bus(void);
 esp_err_t init_spi_devs(void);
 esp_err_t master_transmit_task(void);
 
+//helpers
 spi_transaction_t init_trans(uint8_t *tx_buf, uint8_t *rx_buf, size_t p_size);
 esp_err_t check_bufs(uint8_t* tx_buf, uint8_t* rx_buf, const char* msg);
+esp_err_t scale_buf_alloc(uint8_t** tx_buf, uint8_t** rx_buf, size_t n_bufs, size_t bytes);
 
 
